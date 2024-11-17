85bf894f9ccc8fe65a39db0af5a99a5da4311f39
Dikiy Perec <hopabsq@mail.ru>
2024-11-18 02:28:07 +0600

    another rizz

--- components/shared/header.jsx
+++ components/shared/header.jsx
@@ -32,10 +32,8 @@ export default function Header() {
 												className='flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md'
 												href='/'
 											>
-												<div className='mb-2 mt-4 text-lg font-medium'>shadcn/ui</div>
-												<p className='text-sm leading-tight text-muted-foreground'>
-													Slipe omg best app forever cuz rizz
-												</p>
+												<div className='mb-2 mt-4 text-lg font-medium'>Slipe</div>
+												<p className='text-sm leading-tight text-muted-foreground'>Slipe omg best app forever cuz rizz</p>
 											</a>
 										</NavigationMenuLink>
 									</li>
@@ -52,10 +50,8 @@ export default function Header() {
 												className='flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md'
 												href='/'
 											>
-												<div className='mb-2 mt-4 text-lg font-medium'>shadcn/ui</div>
-												<p className='text-sm leading-tight text-muted-foreground'>
-													Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.
-												</p>
+												<div className='mb-2 mt-4 text-lg font-medium'>Slipe</div>
+												<p className='text-sm leading-tight text-muted-foreground'>Slipe omg best app forever cuz rizz</p>
 											</a>
 										</NavigationMenuLink>
 									</li>
