f2425495d1c54b499aaf3d00a1393df686bb7d8d
Dikiy Perec <hopabsq@mail.ru>
2024-11-22 21:28:34 +0600

    added cdn support

--- components/shared/home/reactions.jsx
+++ components/shared/home/reactions.jsx
@@ -1,12 +1,19 @@
+import Image from "next/image";
+
 export default function Reactions() {
+    const emojiArray = Array.from({ length: 15 }, (_, i) => i + 1).sort(() => Math.random() - 0.5).slice(0, 7)
 	return (
-		<section className='w-full pt-[8.25rem] bg-black flex flex-col gap-6 py-32'>
+		<section className='w-full pt-[8.25rem] overflow-hidden bg-black flex flex-col gap-6 py-32'>
 			<p className='text-[5rem] leading-[6.5rem] font-bold text-white text-center'>
 				React it. Emoji it.
 				<br />
 				<span className='opacity-50'>Instead boring likes.</span>
 			</p>
-            
+            <div className="w-full flex justify-center gap-14">
+                {emojiArray?.map((number, index) => (
+                    <Image alt="emoji" key={index} src={`https://cdn.slipe.fun/site/static/emoji/${number}.png`} width={256} height={256}/>
+                ))}
+            </div>
 		</section>
 	);
 }
