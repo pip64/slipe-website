e53ce39482a08803031d36980040971e4c98c2d4
Dikiy Perec <hopabsq@mail.ru>
2024-11-25 22:32:07 +0600

    improved dev's card animation

--- components/shared/team/card.jsx
+++ components/shared/team/card.jsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 
 export default function DeveloperCard({ name = "", role = "", contact = "", avatar = "", color = "" }) {
 	return (
-		<div className='w-full h-80 max-w-[38.75rem] relative overflow-hidden rounded-[1.75rem]'>
+		<div className='w-full h-80 max-w-[38.75rem] relative overflow-hidden group rounded-[1.75rem]'>
 			<Link
 				href={contact}
 				style={{ "--developer-color": color, "--developer-end-color": `${color}00` }}
-				className='w-full h-full justify-between duration-200 ease-out group hover:bg-[--developer-color] bg-gradient-to-t from-[--developer-color] to-60% to-[--developer-end-color] relative z-10 flex items-end p-7'
+				className='w-full h-full justify-between duration-200 ease-out group-hover:bg-[--developer-color] bg-gradient-to-t from-[--developer-color] to-60% to-[--developer-end-color] relative z-10 flex items-end p-7'
 			>
 				<div className='flex flex-col gap-1'>
 					<span className='text-4xl font-semibold text-white'>{name}</span>
@@ -18,7 +18,7 @@ export default function DeveloperCard({ name = "", role = "", contact = "", avat
 					Contact
 				</Button>
 			</Link>
-			<Image src={avatar} width={640} height={640} className='object-cover top-0 absolute h-full w-full' alt={name} />
+			<Image src={avatar} width={640} height={640} className='object-cover top-0 duration-200 ease-out group-hover:scale-105 absolute h-full w-full' alt={name} />
 		</div>
 	);
 }
--- components/ui/header/header-trigger.jsx
+++ components/ui/header/header-trigger.jsx
@@ -1,7 +1,7 @@
 import { clsx } from "clsx";
 import { Button } from "../button";
 
-export default function HeaderTrigger({ isExpanded, setIsExpanded }) {
+export default function HeaderTrigger({ isExpanded = false, setIsExpanded }) {
 	return (
 		<Button onClick={() => setIsExpanded(!isExpanded)} className='hidden max-md:block' size='icon' variant='icon'>
 			<svg width='24' height='24' viewBox='0 0 24 24' className='!w-10 !h-10' fill='none' xmlns='http://www.w3.org/2000/svg'>
