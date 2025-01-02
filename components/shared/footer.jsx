baa8d7e2db903670f65299e76359e793e6351b6c
pip64 <76626915+pip64@users.noreply.github.com>
2025-01-03 00:22:15 +0700

    Update footer.jsx

--- components/shared/footer.jsx
+++ components/shared/footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
 		<footer className='w-screen p-3 max-md:px-8 max-xl:px-12 px-64 bg-black'>
 			<nav className='w-full text-white flex items-center justify-between gap-7'>
 				<HeaderLogo />
-                <span className="text-md text-white/50">2023-2024 all rights reserved</span>
+                <span className="text-md text-white/50">2023-2025 all rights reserved</span>
 			</nav>
 		</footer>
 	);
