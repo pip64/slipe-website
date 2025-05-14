251cb54179f43370afc8937d9b92329ff5f94eb7
pip64 <76626915+pip64@users.noreply.github.com>
2025-05-14 17:01:35 +0700

    Update hero.jsx

--- components/shared/home/hero.jsx
+++ components/shared/home/hero.jsx
@@ -43,7 +43,7 @@ export default function Hero() {
 					</Button>
 				</DownloadDialog>
 				<Button className='hidden max-md:block' size='dxl'>
-					<Link href='https://cdn.slipe.fun/site/static/app/slipe-1.3.1.apk'>Try app</Link>
+					<Link href='https://cdn.slipe.fun/site/static/app/slipe-1.3.2.apk'>Try app</Link>
 				</Button>
 				<Button size='dxl'>
 					<Link href='https://cdn.slipe.fun/site/static/app/slipe-proto.apk'>Try prototype</Link>
